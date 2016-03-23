d5e14479a6cfe00f354b69bcb08cd2f79933330a
Eric Kwon <mekwon@0587341286.wireless.umich.net>
2016-03-22 21:34:14 -0400

    added braces problem

---
+++ braces.cpp
@@ -0,0 +1,52 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+// Returns the number of balanced braces in a given expression
+// Valid "braces" are (), [], and {}
+int balancedBraces(string expression) {
+	stack<char> s;
+	int size = expression.size();
+
+	// There can be no balanced braces with less than 2 characters
+	if (size < 2)
+		return 0;
+
+	int counter = 0;
+	for (int i = 0; i < size; ++i) {
+		if ((expression[i] == '{') || (expression[i] == '(') || 
+													(expression[i] == '[')) 
+			s.push(expression[i]);
+
+		else if (!s.empty()) {
+			if (expression[i] == '}') {
+				if (s.top() == '{') {
+					++counter;
+					s.pop();
+				}
+				else 
+					return counter;
+			}
+
+			else if (expression[i] == ']') {
+				if (s.top() == '[') {
+					++counter;
+					s.pop();
+				}
+				else 
+					return counter;
+			}
+
+			else if (expression[i] == ')') {
+				if (s.top() == '(') {
+					++counter;
+					s.pop();
+				}
+				else
+					return counter;
+			}
+		}
+	}
+	return counter;
+}
